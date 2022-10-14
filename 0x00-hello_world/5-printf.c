7876ccf26a27109d66de1e36fd46d546f9282217
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-10-13 17:46:31 -0700

    Update Repository

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the sentence
+ * return: 0 if exited properly
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
