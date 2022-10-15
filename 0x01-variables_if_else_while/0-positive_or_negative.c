daf3fdc402b8fc86a9cfe8d4bddd3be4d6ad5ef5
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-10-15 05:41:25 -0700

    Update task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,9 +20,12 @@ int main(void)
 	{
 		printf("%d is zero\n", n);
 	}
-	else
+	else if(n < 0)
 	{
 		printf("%d is negative\n", n);
+	}
+	else {
+		printf("%d is invalid\n", n);
 	}
 	return (0);
 }
