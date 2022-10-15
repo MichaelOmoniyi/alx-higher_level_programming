ce8fe208e4a6e459ef86b88e22d81b445f5a44f0
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-10-15 08:46:17 -0700

    Update task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - executes statement
+ * Return: 0 when succesful
+*/
+
+int main(void)
+{
+	unsigned int a;
+
+	a = 0;
+
+	while (0 < 10)
+	{
+		putchar (a);
+		a++;
+	}
+	putchar ('\n');
+	return (0);
+}
