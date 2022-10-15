4409e5949bfeaf7dc6198b99be06769cb43c1c3b
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-10-15 08:36:51 -0700

    Update task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - execute statements
+ * Return: 0
+*/
+
+int main(void)
+{
+	int a;
+
+	a = 0;
+
+	while (a < 10)
+	{
+		printf("%d", a);
+		a++;
+	}
+	printf("\n");
+	return (0);
+}
