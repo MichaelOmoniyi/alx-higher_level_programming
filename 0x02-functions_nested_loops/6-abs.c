f3c40ccff7125d9f398e2e5a051ae561d46ad09e
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-10-19 14:26:34 -0700

    Update file 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value of an integer
+ * @n: character inputted
+ * Return: 0, when successful
+ */
+
+int _abs(int n)
+{
+	_putchar ('n');
+	return (0);
+}
