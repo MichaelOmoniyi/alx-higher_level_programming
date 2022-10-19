8805a28a25c2453abf690a8e375faeaaea3c6b6b
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-10-19 16:20:28 -0700

    Update task 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and return the result
+ *
+ * Return: 0, when successful
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	_putchar (c + '0');
+	return (0);
+}
