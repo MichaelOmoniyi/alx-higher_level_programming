0c2d7b32b1d03552b0211495413cff4628fb944c
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-10-19 15:30:59 -0700

    Update file 9-time_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_tabel - prints 9 times table
+ */
+
+void times_tabel(void)
+{
+	int a, b, c;
+	c = a * b;
+
+	for (a = 0; a <= 0, a++)
+	{
+		for (b = 0; b <= 0; b++)
+		{
+			if ((c / 10) == 0)
+			{
+				if (b != 0)
+					_putchar (' ');
+				_putchar (n + '0');
+
+				if (b == 9)
+					continue;
+				_putchar (',');
+				_putchar (' ');
+			}
+			else
+			{
+				_putchar ((c / 10) + '0');
+				_putchar ((c % 10) + '0';
+				if (b == 9)
+					continue;
+				_putchar (',');
+				_putchar (' ');
+			}
+		}
+	}
+	_putchar ('\n');
+}
+
