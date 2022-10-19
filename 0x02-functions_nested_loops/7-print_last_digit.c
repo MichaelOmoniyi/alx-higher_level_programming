7c6e69fac43240edfc65c770ec0bb7e07e72f14d
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-10-19 14:43:10 -0700

    Update file 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,5 +10,12 @@ int print_last_digit(int n)
 {
 	int lastDigit = n % 10;
 
+	if (n < 0)
+		n = -n;
+
+	if (lastDigit < 0)
+		lastDigit = -lastDigit;
+
+	_putchar(lastDigit + '0');
 	return (lastDigit);
 }
