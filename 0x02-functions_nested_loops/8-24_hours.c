0ef575b2eea69b80187a238e99777376762e21f3
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-10-19 15:02:07 -0700

    Update file 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ */
+
+void jack_bauer(void)
+{
+	int b, j;
+
+	for (b = 0; b < 23; b++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			if (b != 23 && j != 59)
+			{
+				_putchar ((b / 10) + '0');
+				_putchar ((b % 10) + '0');
+				_putchar (':');
+				_putchar ((j / 10) + '0');
+				_putchar ((j % 10) + '0');
+				_putchar ('\n');
+			}
+			else
+			{
+				continue;
+			}
+		}
+	}
+}
