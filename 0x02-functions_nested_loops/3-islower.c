db95a7273a43c9e9bf0edca22813d842f235fe2e
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-10-19 07:40:07 -0700

    Update file 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+inclued "main.h"
+
+/**
+ * islower - checks for lower case character
+ *
+ * Return: 0
+ */
+
+int _islower (int c)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch = 'c')
+			return (ch);
+		else
+			return (0);
+	}
+}	
