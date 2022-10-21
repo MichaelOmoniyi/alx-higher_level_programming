f91e85112009f068ff49197a55f646ecfcf13597
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-10-21 06:20:11 -0700

    Update file 100-times_table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * print_times_alphabet - prints the n times table
+ * @n: Input character
+ */
+
+void print_times_table(int n)
+{
+	int a, b , c;
+	for (a = 0; a <= 15; a++)
+	{
+		for (b = 0; b <= 15; b++)
+		{
+			c = a * b;
+
+			if (a = 0)
+			{
+				_putchar (c + '0');
+			}
+			if (c < 10)
+			{
+				_putchar (c + '0');
+				_putchar (',');
+				_putchar (' ');
+			}
+			if (c >= 10 && c < 100)
+			{
+				_putchar ((c / 10) + '0');
+				_putchar ((c % 10) + '0');
+				_putchar (',');
+				_putchar (' ');
+			}
+			if (c >= 100)
+			{
+				_putchar ((c / 100) + '0');
+				_putchar (((c / 10) % 10) + '0');
+				_putchar (c % 10) + '0');
+				_putchar (',');
+				_putchar (' ');
+			}
+		}
+	}
+	else
+	{
+		_putchar ('\n');
+	}
+}
