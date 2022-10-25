7b354ce3c687b47fdd799c34c7f5eac863e15ef5
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-10-25 00:47:07 -0700

    Update file 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: input pointer parameter
+ * @b: input pointer parameter
+ */
+
+void swap_int(int *a, int *b)
+{
+	int str1;
+	int str2;
+
+	*a = str1;
+	*b = str2;
+
+	strcpy(str1, str2);
+}
