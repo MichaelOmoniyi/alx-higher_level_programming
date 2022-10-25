1c00b16fd8b8d9b7bab763c7871255d9b9aa1b70
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-10-25 08:06:19 -0700

    Update file 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,11 +11,11 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	for (i = n; i < n; i++)
+	for (; i < n; i++)
 	{
 		printf("%d", a[i]);
 		if (i < n - 1)
 			printf(", ");
 	}
-	_putchar ('\n');
+	putchar ('\n');
 }
