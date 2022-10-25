574c517017dd3610643eff3bbaad1d11ea7aacc8
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-10-25 01:01:54 -0700

    Update file 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: input character parameter
+ */
+
+int _strlen(char *s)
+{
+	char str;
+	int len;
+
+	str = *s;
+
+	len = strlen(str);
+}
