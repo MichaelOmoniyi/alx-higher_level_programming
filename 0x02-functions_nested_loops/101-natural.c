1f2f209b881283b2bb94c06e7be7e6fa290574f9
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-10-31 02:22:33 -0700

    Update file 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int n, sum = 0;
 
-	for (n = 3; n < 1024; n++)
+	for (n = 0; n < 1024; n++)
 	{
-		if (n % 3 == 0 && n % 5 == 0)
+		if (n % 3 == 0 || n % 5 == 0)
 		{
 			sum += n;
 		}
