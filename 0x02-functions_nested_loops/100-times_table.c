08a33a837c54a45a044470a123ed8ea26cdb10a7
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-10-31 01:33:19 -0700

    Update file 100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,9 +11,9 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (a = 0; a <= 15; a++)
+		for (a = 0; a <= n; a++)
 		{
-			for (b = 0; b <= 15; b++)
+			for (b = 0; b <= n; b++)
 			{
 				c = a * b;
 
