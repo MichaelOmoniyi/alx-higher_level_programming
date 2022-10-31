ce99ebdbe919ff39753fabc28dca99ea07cfb1c1
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-10-31 01:51:07 -0700

    Update file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,10 +24,13 @@ void print_times_table(int n)
 				{
 					_putchar (',');
 					_putchar (' ');
+					_putchar (' ');
+					_putchar (' ');
 					_putchar (c + '0');
 				} else if (c >= 10 && c < 100)
 				{
 					_putchar (',');
+					_putchar (' ');
 					_putchar (' ');
 					_putchar ((c / 10) + '0');
 					_putchar ((c % 10) + '0');
