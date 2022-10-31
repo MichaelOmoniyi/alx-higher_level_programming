6585b271ea9bee631ba82b3f3480a545ccc0f65b
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-10-31 02:09:42 -0700

    Update file 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of add multiples of 3 or 5 below 1024
+ *
+ * Return: 0 when successful
+ */
+
+int main (void)
+{
+	int n
+
+	for (n = 3; n < 1024; n++)
+	{
+		if (n / 3 = 0 && n / 5 = 0)
+		{
+			printf("%d, \n", n);
+		}
+	}
+	Return (0);
+}
