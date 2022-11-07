7c49a16f6d33ba9a438d4f367321d9af59bb154a
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-11-07 00:14:36 -0800

    Update file 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments passed into the program
+ * @argc: The nummber of arguments passed into the program
+ * @argv: Pointer to the arguments passed into the program
+ * Return: 0 when successful
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
