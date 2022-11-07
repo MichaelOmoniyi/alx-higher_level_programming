e17080c18046418f0cf9cc550196d055a6168f2f
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-11-07 00:06:44 -0800

    Update file 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program name, followed by a new line.
+ * @argc: The number of argument supplied to the program
+ * @argv: An array of pointers to the argument
+ * Return: 0 when successful
+ */
+
+int main(int argc, char *argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
