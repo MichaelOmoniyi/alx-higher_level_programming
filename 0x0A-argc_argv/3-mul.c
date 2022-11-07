5066c8a0e47dddac991216ef0e3b261e7ab15cf6
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-11-07 00:44:34 -0800

    Update file 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,17 +11,17 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2, multi;
-	
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	
+
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 	multi = num1 * num2
-	
+
 	printf("%d\n", multi);
 
 	return (0);
