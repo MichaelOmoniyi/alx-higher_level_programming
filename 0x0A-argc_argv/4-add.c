347c0bbc8121112c4643cd1b908c8ff1f3be2414
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-11-07 01:02:19 -0800

    Update file 3-mul.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: The number of arguments supplied into the program
+ * @argv: Pointer to the arguments the program receives
+ *
+ * Return: 0, if program receives two argument
+ * 1, if program receives one argument
+ */
+
+int main(int argc, char *argv[])
+{
+	int num, digit, add = 0;
+
+	for (num = 1; num < argc, num++)
+	{
+		for (digit = 0; argv[num][digit]; digit++)
+		{
+			if (argv[num][digits] < '0'  || argv[num][digi] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		add += atoi(argv[num]);
+	}
+
+	printf("%d\n", add;
+
+	return (0);
+}
