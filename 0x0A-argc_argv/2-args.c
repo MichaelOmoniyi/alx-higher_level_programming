bc96187ac62cdd51a8cb444904db0d90ec75d44d
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-11-07 00:23:57 -0800

    Update file 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: The number of arguments supplied into the program
+ * @argv: Pointer to the arguments the program receives
+ *
+ * Return: 0 when successful
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0, i < argc, i ++)
+	{
+		printf("%s\n", argv[i]);
+
+		return (0);
+	}
+}
