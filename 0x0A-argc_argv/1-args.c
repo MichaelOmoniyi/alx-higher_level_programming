0b1afced14411aeb6a993117126e5096af083294
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-11-07 00:19:07 -0800

    Update file 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,9 +7,9 @@
  * Return: 0 when successful
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((__unsused__)) *argv[])
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
