5f4d1a66272f7db1aa0352e8106ba84845744d81
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-12-31 18:29:32 +0100

    Update task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,14 +12,14 @@ int n, i;
 
 for (n = 0; n < 10; n++)
 {
-for (i = 0; i <= 14; i++)
+for (i = 0; i < 15; i++)
 {
 if (i > 9)
 {
 _putchar(i / 10 + '0');
 _putchar(i % 10 + '0');
 }
-_putchar('\n');
 }
+_putchar('\n');
 }
 }
