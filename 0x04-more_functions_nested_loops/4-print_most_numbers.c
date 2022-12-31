cf83783d56cf1891c1c2bf9cc9f94264ebdf652d
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-12-31 18:07:37 +0100

    Update task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Print numbers 0 to 9 without the 
+ * print_most_numbers - Print numbers 0 to 9 without the
  * numbers 2 and 4
  *
  * Return: Nothing
@@ -13,7 +13,7 @@ int n;
 
 for (n = '0'; n <= '9'; n++)
 {
-if (n = '2' || n = '4')
+if (n == '2' || n == '4')
 continue;
 else
 _putchar(n);
