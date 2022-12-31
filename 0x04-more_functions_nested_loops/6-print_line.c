4039ec761ea47f8928dcedf055af9adf0601d05a
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2022-12-31 18:48:21 +0100

    Update task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line on the terminal.
+ *
+ * Return: Nothing
+ */
+
+void print_line(int n)
+{
+int i;
+for (i = 0; i <= n; i++)
+{
+_putchar('_');
+}
+_putchar("\n");
+_putchar('\n');
+}
