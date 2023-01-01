7361219b3cbf7642ea20f66905928a6481037fca
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-01-01 14:46:57 +0100

    Update advance task

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -7,7 +8,7 @@
  */
 int main(void)
 {
-unsigned long int i = 3; n = 612852475143;
+unsigned long int i = 3, n = 612852475143;
 for (; i < 12057; i += 2)
 {
 while (n % i == 0 && n != i)
