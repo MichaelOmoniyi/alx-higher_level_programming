b0be5857cd683423b76d90ef920910bbdf7baf66
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-01-01 13:37:37 +0100

    Update task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,16 +15,16 @@ int num;
 for (num = 0; num <= 100; num++)
 {
 if (num % 3 == 0 && num % 5 == 0)
-    printf("FizzBuzz");
+printf("FizzBuzz");
 else if (num % 3 == 0)
-    printf("Fizz");
+printf("Fizz");
 else if (num % 5 == 0)
-    printf("Buzz");
+printf("Buzz");
 else
-    printf("%d", num);
+printf("%d", num);
 
 if (num != 100)
-    printf(" ");
+printf(" ");
 }
 printf("\n");
 return (0);
