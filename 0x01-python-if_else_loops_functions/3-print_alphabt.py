24ae204a40403d5f1f021780c2796a7bcbc26749
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-01-04 03:32:17 +0100

    Update file 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for letters in range(97, 123):
-    if letters == e or letters == q
-        continue
+    if letters == e or letters == q:
+    continue
 
     print("{:c}".format(letters), end="")
