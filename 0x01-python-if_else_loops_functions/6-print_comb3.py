474c1627c135765f2b4983cb02be49efb9eb367b
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-01-04 04:19:11 +0100

    Update file 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 for count1 in range(0, 10):
-    for count2 in range(0, 10):
-        if count1 == 9 and count2 == 9:
+    for count2 in range(count1 + 1, 10):
+        if count1 == 8 and count2 == 9:
             print("{:d}{:d}".format(count1, count2))
-        elif count1 == 1 and count2 == 0:
-            continue
         else:
             print("{:d}{:d}, ".format(count1, count2), end="")
