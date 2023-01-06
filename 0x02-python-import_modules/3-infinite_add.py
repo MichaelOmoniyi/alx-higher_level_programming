dba799e680249ab1927e04e3e8e0c23395f281c6
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-01-06 03:51:45 +0100

    Update file 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,13 +2,13 @@
 import sys
 
 if __name__ == "__main__":
+    exit()
 
-    result = 0
-    i = 0
-
-    for argument in sys.argv:
-        if i != 0:
-            result += int(argument)
-        else:
-            i += 1
-    print("{:d}.format(result))
+i = 0
+result = 0
+for argument in sys.argv:
+    if i != 0:
+        result += int(argument)
+    else:
+        i += 1
+print("{:d}".format(result))
