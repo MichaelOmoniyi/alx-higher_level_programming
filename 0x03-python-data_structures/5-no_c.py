21b56f288db1d1e45f6deea61d7418d644d207da
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-01-10 02:40:29 +0100

    Update file 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    my_string.remove('c', 'C')
-    return (my_string)
+    new_string = ""
+
+    for i in my_string:
+        if i not in my_string:
+            new_string += i
+    return (new_string)
