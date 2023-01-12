44768c7e31c5971e5dce696893cfcf8d42b868a3
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-01-12 21:01:18 +0100

    Update file 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 
 def print_sorted_dictionary(a_dictionary):
     for i in sorted(a_dictionary):
-       print("{:s}: {}".format(i, a_dictionary[i]))
+        print("{:s}: {}".format(i, a_dictionary[i]))
