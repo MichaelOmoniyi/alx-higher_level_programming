30869f2a2f1678c37f700b2710af76aed1fb9edb
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-01-12 14:02:20 +0100

    Update file 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,9 +2,9 @@
 
 def square_matrix_simple(matrix=[]):
     new_matrix = []
+    new_row = []
     for row in matrix:
         for i in row:
-            new_row = []
             new_row.append(i ** 2)
         return (new_row)
         new_matrix.append(row)
