3c159340c239750ea2e941b3535c5425e9df1c58
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-01-12 21:20:04 +0100

    Update file 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    if isinstance(key, str):
+    if type(key) == str:
         del a_dictionary[key]
         return (a_dictionary)
     return (a_dictionary)
