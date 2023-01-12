54bddc44d4ccf439a388b711409b06e50bd19b2e
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-01-13 00:50:35 +0100

    Update file 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,6 +4,5 @@ def multiply_by_2(a_dictionary):
     new_dict = a_dictionary.copy()
 
     for i, j in new_dict.items:
-        print("{:s}: {}".format(i, j ** 2))
-#        new_dict[i] = j
-#    return (new_dict)
+        new_dict[i] = j ** 2
+    return (new_dict)
