05d958e6d17da82171524a7eb2c3a73170ab2b1d
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-01-13 04:29:27 +0100

    Update file 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
     romans = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     result = 0
 
-    if not roman_string or type(roman_string) == str:
+    if not roman_string or type(roman_string) != str:
         return 0
     for i in range(len(roman_string)):
         if i > 0 and romans[roman_string[i]] > romans[roman_string[i - 1]]:
