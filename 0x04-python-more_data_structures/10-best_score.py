dce29820ac7711ed171f292d730e0b28e94dd6f9
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-01-13 02:37:03 +0100

    Update file 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    best = list(reversed(sorted(a_dictionary.values())))
-    if a_dictionary == 'None' or len(a_dictionary) == 0:
+    if a_dictionary:
+        best = list(reversed(sorted(a_dictionary.values())))
+        for i in a_dictionary:
+            if a_dictionary[i] == best[0]:
+                return (i)
+    else:
         return None
-    for i in a_dictionary:
-        if a_dictionary[i] == best[0]:
-            return (i)
-    return i
 #        for i, j in a_dictionary.items():
 #            if j == best[0]:
 #                return i
