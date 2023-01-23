305541a214ecd1e1442cdeff6cb42696c463710c
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-01-23 07:33:59 -0800

    Update files 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
 
     for i in range(x):
         try:
-            print(f"{my_list[i]}", end=" ")
+            print(f"{my_list[i]}", end="")
             length += 1
         except IndexError:
             break
