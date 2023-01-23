bfa64fa3ba7e7482fcb96187b7bac6b391b802da
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-01-23 07:56:25 -0800

    Update file 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,8 +2,8 @@
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(value), end="")
+        print("{:d}".format(value))
         return True
     except (ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return False
-        raise ValueError
