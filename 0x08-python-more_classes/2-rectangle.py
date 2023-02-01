1b1f15040b8d3e635ff429f160cc2a2c0686dca0
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-02-01 16:17:58 +0100

    Update file 2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -51,4 +51,6 @@ class Rectangle:
 
     def perimeter(self):
         """returns the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
         return (self.__width * 2) + (self.__height * 2)
