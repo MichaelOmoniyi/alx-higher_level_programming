d2afc658e9aa602e5045624be29b9e66bbe0293f
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-02-06 01:13:50 +0100

    Update file 6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -49,6 +49,7 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__height = value
+    
     def area(self):
         """ This returns the area of the rectangle"""
         return (self.__width * self.__height)
@@ -59,7 +60,7 @@ class Rectangle:
             return (perimeter)
         perimeter = (2 * self.__width) + (2 * self.__height)
         return perimeter
-    
+
     def __str__(self) -> str:
         """ This diagramatically shows a rectangle"""
         if self.__width == 0 or self.__height == 0:
