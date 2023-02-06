4a26aa303ed8ea69eaf9db56f4db28acde588ecb
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-02-07 00:18:40 +0100

    Update file 9-rectangle.py

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""This is a subclass of the BaseGeometry class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """This a class called Rectangle"""
+
+    def __init__(self, width, height):
+        """This initializes the subclass Rectangle"""
+
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Returns the Rectangle area"""
+        return (self.__width * self.__height)
+
+    def __str__(self) -> str:
+        """Return a description of the rectangle"""
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
