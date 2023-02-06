9f66a4471cd0801e7d84bc70505de7d66b50ba95
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-02-06 17:13:53 +0100

    Update file 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" This is class calculates basic geometry"""
+
+
+class BaseGeometry:
+    """ This class return values for basic geometry"""
+
+    def area(self):
+        """ Raises an Exception"""
+
+        raise Exception("area() is not implemented")
