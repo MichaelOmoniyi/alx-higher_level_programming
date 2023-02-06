15e0955de158d1f85449eeb0dbe56df39a330dff
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-02-07 00:52:51 +0100

    Update file 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -8,7 +8,7 @@ class MyInt(int):
 
     def __eq__(self, value):
         """Override == operator with != operator"""
-        return self.__real != value
+        return self.real != value
 
     def __ne__(self, value):
         """Override != operator with == operator"""
