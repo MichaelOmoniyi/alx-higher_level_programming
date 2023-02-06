4f50cd83c84c0224d09aa81edd08022f1fec04e3
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-02-06 16:32:23 +0100

    Update files

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 """ The Mylist class is inherited from a class list"""
-class Mylist(list):
-    """ This class prints a sorted list"""
 
+
+class MyList(list):
+    """ This class prints a sorted list"""
     def print_sorted(self):
-        list.sort()
-        print(list)
+        """ Prints a sorted list"""
+        print(sorted(self))
