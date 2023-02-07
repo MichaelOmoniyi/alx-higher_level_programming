7b30596b132958de5482d617d6dfd2c5dfedb23e
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-02-07 23:39:08 +0100

    Update file 1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""This module defines a text-file editor"""
+
+
+def write_file(filename="", text=""):
+    """This function writes a string to a text file (UTF8) and
+    returns the number of characters written
+    """
+    with open(filename="", text="") as f:
+        f.write(text)
