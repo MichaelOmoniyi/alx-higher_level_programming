fc45a0e96a6f44fb53b237b006733a2e37b9d8c5
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-02-08 00:34:57 +0100

    Update file 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     except FileNotFoundError:
         items = []
     items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item_json")
+    save_to_json_file(items, "add_item.json")
