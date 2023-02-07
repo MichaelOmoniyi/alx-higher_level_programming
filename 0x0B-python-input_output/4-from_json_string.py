00d7584237c67f98967c76c0c5d803772db9ea95
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-02-08 00:03:56 +0100

    Update file 4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines JSON representation"""
+import json
+
+
+def from_json_string(my_str):
+    """This function returns an object (Python data structure)
+    represented by a JSON string
+    """
+    return json.dumps(my_obj)
