fd18f83448b31ff9c5e44e060fdd52b6242fbda6
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-02-10 14:11:19 +0100

    Update file 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """This module returns dictionary descriptions of objects"""
 
-def class_to_json(obj):
-    """This function returns the dictionary description with simple data structure 
-    (list, dictionary, string, integer and boolean) for JSON serialization of an object"""
 
+def class_to_json(obj):
+    """This function returns the dictionary description
+    with simple data structure (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object
+    """
 
     return dir(obj)
