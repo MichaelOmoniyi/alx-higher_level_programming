ad375faf44a2e7bcff0e0151e38d93f555df8d7b
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-09-14 00:28:23 +0000

    Update files

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,35 @@
+def pascal_triangle(n):
+    """The function returns a list of lists of
+        integers representing the Pascal’s triangle of n
+
+    Args:
+        n (int): The Pascal number
+    """
+
+    triangle = []
+    if n == 0:
+        return triangle
+    else:
+        row = 0
+        while row < n:
+            row_list = []
+            value = 1
+            if row == 0:
+                row_list.append(value)
+                triangle.append(row_list)
+            elif row == 1:
+                row_list.append(value)
+                row_list.append(value)
+                triangle.append(row_list)
+            else:
+                for num in range(row):
+                    if num == 0:
+                        row_list.append(value)
+                    else:
+                        value = (triangle[row - 1][num]) \
+                            + (triangle[row - 1][num - 1])
+                        row_list.append(value)
+                row_list.append(1)
+                triangle.append(row_list)
+            row += 1
+        return triangle
