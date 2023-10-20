dc0da283d56495629a91b29d3780613d398dd95e
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-10-20 09:03:24 +0000

    Make corrections to 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 
 -- Creates the user user user_0d_2
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grants the user SELECT privilege
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
