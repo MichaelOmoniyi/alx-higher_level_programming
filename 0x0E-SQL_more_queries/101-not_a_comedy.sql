0c4017fed0c811005cd5fd5edf46d5c74bb7ffb3
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-10-21 09:12:17 +0000

    Add file for advanced tasks

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,12 @@
+-- Lists all shows without the genre Comedy in the database hbtn_0d_tvshows
+
+SELECT title
+FROM tv_shows
+WHERE title NOT IN (
+    SELECT tv_shows.title
+    FROM tv_shows
+    LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+    LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+    WHERE tv_genres.name = "Comedy")
+GROUP BY title
+ORDER BY tv_shows.title;
