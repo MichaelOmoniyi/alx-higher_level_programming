424db7e562133b383b7364fa9bf195e217dd3aa3
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-11-14 15:04:16 +0000

    Implement semistandard compliance for 3-value_argument.js file

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,7 +4,7 @@ If no arguments are passed to the script, print "No argument" */
 
 const args = process.argv.slice(2);
 if (args.length === 0) {
-    console.log('No argument');
+  console.log('No argument');
 } else {
-    console.log(`${args[0]}`);
+  console.log(`${args[0]}`);
 }
