0da919e4f94fbcd615436e0757a7f5e85dc5b30b
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-11-14 16:12:41 +0000

    Update 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -6,8 +6,9 @@ const myObject = {
 };
 console.log(myObject);
 myObject.incr = function () {
-  this.value++;
+  this.value += 1;
 }
+myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
