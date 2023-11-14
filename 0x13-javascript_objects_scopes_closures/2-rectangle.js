c7fc1c533d074dfe84e5f201d8f454bc3d3a86f8
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-11-14 19:28:33 +0000

    Upload task 3 solution

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* This defines an empty class Rectangle */
+class Rectangle {
+    constructor(w, h) {
+        if ((w > 0) && (h > 0)) {
+            this.width = w;
+            this.height = h;
+        }
+    }
+}
+
+module.exports = Rectangle;
