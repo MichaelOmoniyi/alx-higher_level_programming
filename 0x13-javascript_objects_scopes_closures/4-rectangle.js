ee5cf78eab8ce9e55244f2ba8df55013fb674247
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-11-14 19:29:12 +0000

    Upload task 4 solution

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+/* This defines an empty class Rectangle */
+class Rectangle {
+    constructor(w, h) {
+        if ((w > 0) && (h > 0)) {
+            this.width = w;
+            this.height = h;
+        }
+    }
+
+    print() {
+        const row = 'X'.repeat(this.width);
+        for(let y = 0; y < this.height; y++) {
+            console.log(`${row}`);
+        }
+    }
+
+    rotate() {
+        const aux = this.width;
+        this.width = this.height;
+        this.height = aux;
+    }
+
+    double() {
+        this.width = this.width * 2;
+        this.height = this.height * 2;
+    }
+}
+
+module.exports = Rectangle;
