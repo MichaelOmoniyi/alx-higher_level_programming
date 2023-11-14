d67e9d70443c158b92a9290c1341bda22c08c231
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-11-14 19:30:05 +0000

    Upload task 7 solution

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/* Returns the number of occurences in a list */
+
+exports.nbOccurences = function (list, searchElement) {
+    let count = 0;
+    for(let i = 0; i < list.length; i++) {
+        if (list[i] === searchElement) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+module.exports = { nbOccurences };
