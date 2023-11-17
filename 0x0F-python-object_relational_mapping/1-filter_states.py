64a9e4ad8d3c5d6168285f0b930bd31d085736aa
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-11-17 11:05:44 +0000

    Update solution for task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+username = str(sys.argv[1])
+password = str(sys.argv[2])
+dbName = str(sys.argv[3])
+tableName = "states"
+db = MySQLdb.connect(host='localhost', user=username,
+                     passwd=password, db=dbName)
+dbCur = db.cursor()
+dbCur.execute("""SELECT id, name FROM {} ORDER BY id ASC""".format(tableName))
+results = dbCur.fetchall()
+
+for row in results:
+    if row[1][0] == 'N':
+        print(row)
+cur.close()
+db.close()
