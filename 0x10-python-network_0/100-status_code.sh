7e7e7e19379ecf56df4dbe00d6c704b314030594
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-12-01 14:42:50 +0000

    Update file

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -s -o /dev/null -v "%{http_code}" "$1"
+curl -s -o /dev/null -w "%{http_code}" "$1"
