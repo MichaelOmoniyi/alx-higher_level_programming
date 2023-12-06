13ac027a54955dc79bc07cf1ce2b3a3289563a49
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-12-06 07:11:39 +0000

    Make pycodestyle validator

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ import urllib.request
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
-    
+
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
         html = response.read()
