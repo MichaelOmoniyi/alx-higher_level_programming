66ab474cae41afa32825489d236c2879bc53ff34
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2024-01-03 14:43:23 +0000

    Update solution to task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,9 +2,5 @@
 const request = require('request');
 const url = 'http://swapi.co/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(JSON.parse(body).title);
-  }
+  console.log(error || JSON.parse(body).title);
 });
