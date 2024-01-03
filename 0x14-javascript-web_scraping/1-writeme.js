817c94947fdf702ae1e0594b8342c5766f530fdc
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2024-01-03 14:41:16 +0000

    Update solution to task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,10 +2,8 @@
 const fs = require('fs');
 const fileName = process.argv[2];
 const fileContent = process.argv[3];
-fs.writeFile(fileName, fileContent, 'utf-8', function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(fileContent);
+fs.writeFile(fileName, fileContent, error => {
+  if (error) {
+    console.log(error);
   }
 });
